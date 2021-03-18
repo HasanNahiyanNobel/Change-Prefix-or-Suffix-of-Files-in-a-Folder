40a0aface972a33072214cb249c3617f4bc6177c
Hasan Nahiyan Nobel <43447022+HasanNahiyanNobel@users.noreply.github.com>
2021-03-18 08:34:53 +0600

    Initial commit.

---
+++ remove_prefix_copy_of.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Author: Hasan Nahiyan Nobel
+# Copyright: Attribution 4.0 International (CC BY 4.0)
+# This script removes the prefix "Copy of" from files of the current directory (not recursively)
+
+ls -l
