fa91d511d1a866e2eb0f449c831faf1b6b016257
Hasan Nahiyan Nobel <43447022+HasanNahiyanNobel@users.noreply.github.com>
2021-03-27 12:28:59 +0600

    Added basic GUI.

---
+++ GUI.py
@@ -0,0 +1,6 @@
+from tkinter import *
+
+root = Tk()
+theLabel = Label(root, text="Hey Jude!")
+theLabel.pack()
+root.mainloop() # To make the window "stay", and not vanish immediately after running
